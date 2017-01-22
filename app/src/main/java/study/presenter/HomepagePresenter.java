7ab41282bbe76d93962de96544215ba41dbcd63c
yanglinfeng <4764194@qq.com>
2017-01-22 17:37:54 +0800

    add rxjava2

--- app/src/main/java/study/presenter/HomepagePresenter.java
+++ app/src/main/java/study/presenter/HomepagePresenter.java
@@ -1,14 +1,20 @@
 package study.presenter;
 
 import android.os.SystemClock;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.Random;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 import study.model.bean.FruitBean;
 import study.presenter.contract.HomepageContract;
 import study.sunshine.newTechnique.R;
-import study.ui.home.activity.HomepageActivity;
 import study.ui.home.adapter.HomepageAdapter;
 
 /**
@@ -16,6 +22,7 @@ import study.ui.home.adapter.HomepageAdapter;
  */
 
 public class HomepagePresenter implements HomepageContract.Presenter {
+    private static final String TAG = "HomepagePresenter";
     private HomepageContract.View mView;
 
     private FruitBean[] fruits = {new FruitBean("Apple", R.drawable.apple),
@@ -27,15 +34,21 @@ public class HomepagePresenter implements HomepageContract.Presenter {
 
     @Override
     public void refreshFruits() {
-        new Thread(() -> {
-            SystemClock.sleep(1500);
-            ((HomepageActivity) mView).runOnUiThread(() -> {
-                        initFruitData();
-                        mView.refreshFruitAdapter(mHomepageAdapter);
-                        mView.setRefreshing(false);
-                    }
-            );
-        }).start();
+        Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> e) throws Exception {
+                Log.d(TAG, "subscribe: "+Thread.currentThread().getName());
+                SystemClock.sleep(1500);
+                initFruitData();
+                e.onNext("success");
+            }
+        }).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                mView.refreshFruitAdapter(mHomepageAdapter);
+                mView.setRefreshing(false);
+            }
+        });
     }
 
     @Override
